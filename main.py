00380c867ea131e09a2fb3786b9cb531cc20f0dc
neeleshbisht99 <neelesh@aidash.com>
2023-06-11 08:08:25 +0530

    added gitignoreg

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+from fastapi.middleware.cors import CORSMiddleware
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+openai.api_key = "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"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = FastAPI()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can replace "*" with specific origins
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+class Task(BaseModel):
+    id: int
+    title: str
+    description: str
+    ai_advice: str = ""
+    done: bool
+
+class Conversation(BaseModel):
+    text: str
+
+tasks = []
+
+@app.get("/tasks/")
+async def read_tasks():
+    return tasks
+
+
+@app.post("/chat/")
+async def converse(conversation:Conversation):
+    response = openai.ChatCompletion.create(
+      model = "gpt-3.5-turbo",
+      temperature = 0.2,
+      max_tokens = 3000,
+      messages = [
+        {"role": "user", "content": conversation.text}
+      ]
+    )
+    return response
